f251829efe8ccce93e5bc167abb0988c87dd2ba1
MystiaFin <cytrsx01@gmail.com>
2025-04-10 08:57:53 +0700

    home screen done

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,17 @@ function App() {
 
   return (
     <div
-      className="min-h-screen bg-cover bg-center bg-no-repeat"
+      className="fixed inset-0 flex items-center justify-center bg-cover bg-center bg-no-repeat -z-15"
       style={{ backgroundImage: `url(${homeBackground})` }}
     >
-      <div className="flex flex-col justify-center container mx-auto px-4 pt-5">
-        <div className="flex flex-col justify-center items-center text-center mt-6">
-          <h2 className="font-black text-4xl sm:text-2xl md:text-3xl mb-2">
+      <div className="flex flex-col justify-center items-center container mx-auto">
+        <div className="flex flex-col justify-center items-center text-center">
+          <div className="absolute w-64 h-64 rounded-full bg-white/30 blur-xl radial-gradient -z-10"></div>
+          <h2 className="font-black text-4xl sm:text-2xl md:text-3xl mb-2 pb-[4%]">
             Welcome
           </h2>
-          <h3 className="font-medium text-2xl mb-2">to</h3>
-          <span className="px-2 py-1 rounded-full bg-white/50 font-medium text-2xl text-[#FAAC01] mb-2">
+          <h3 className="font-medium text-2xl mb-2 pb-[4%]">to</h3>
+          <span className="px-2 py-1 rounded-full bg-white/50 font-medium text-2xl text-[#FAAC01] mb-2 pb-[4%]">
             DynooBoo Store
           </span>
         </div>
@@ -28,22 +29,22 @@ function App() {
           <img
             src={homeLogo}
             alt="Home Logo"
-            className="w-[80%] h-auto sm:max-w-xs"
+            className="w-[75%] h-auto mt-5 mb-5 pb-[4%]"
           />
         </div>
-        <div className="flex justify-center items-center gap-4">
+        <div className="flex justify-center items-center gap-4 pb-[4%]">
           <button className={buttonStyle}>Sign In</button>
           <button className={buttonStyle}>Sign up</button>
         </div>
-        <span className="text-[#F7A600] font-bold text-center mt-4">
+        <span className="text-[#F7A600] font-bold text-center mt-4 pb-[4%]">
           Skip for now
         </span>
-        <div className="flex justify-center items-center gap-4 mt-6">
+        <div className="flex justify-center items-center gap-4 mt-6 pb-[4%]">
           {authContent.map((icon: any, index) => (
             <a
               href="#"
               key={index}
-              className="hover:opacity-80 transition-opacity"
+              className="hover:opacity-80 transition-opacity pb-[4%]"
             >
               <img
                 src={icon.default}
@@ -53,6 +54,7 @@ function App() {
             </a>
           ))}
         </div>
+        <div className="absolute bottom-0 left-0 w-full h-[35%] bg-gradient-to-b from-white/40 to-transparent -z-10"></div>
       </div>
     </div>
   );
