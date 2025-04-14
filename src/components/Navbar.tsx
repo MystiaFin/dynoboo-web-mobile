ab8f57a8c5b90c633996c764844a5875f44995ca
MystiaFin <cytrsx01@gmail.com>
2025-04-14 10:23:03 +0700

    landing page finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,22 @@
+const hamburgerStyle: string =
+  "mb-[6px] w-[28px] h-[8px] block bg-[#3A603B] rounded-r-xs";
+
 const Navbar = () => {
   return (
-    <div>
+    <div className="font-secondary">
       {/* Hamburger Menu Button */}
-      <div>
-        <button>
-          <span className="w-4 h-2 bg-green-900"></span>
-          <span></span>
-          <span></span>
+      <div className="flex mt-[10%] items-center gap-7">
+        <button className="flex flex-col items-center justify-center">
+          {[0, 1, 2].map((index) => (
+            <span key={index} className={hamburgerStyle}></span>
+          ))}
         </button>
+        <div className="flex flex-col mb-2">
+          <span>DynoBoo Logo</span>
+          <span className="text-xs">Slogan</span>
+        </div>
       </div>
-      <div>{/* Nav Content */}</div>
+      <nav></nav>
     </div>
   );
 };
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,30 @@
 import Navbar from "../components/Navbar";
+import dynoRight from "../assets/landing/dyno-right.png";
+import dynoLeft from "../assets/landing/dyno-left.png";
 
 const Landing = () => {
   return (
-    <div>
+    <div className="font-secondary">
       <Navbar />
+      <div className="ml-[8%] mt-[14%] flex flex-col">
+        <span className="font-bold text-4xl mb-4 text-[#5a7e51]">Handmade</span>
+        <span className="font-bold text-6xl text-[#3A603B] ">Craftshop</span>
+        <button className="mt-[40%] w-40 h-15 bg-[#3A603B] text-white py-3 px-8 rounded-full font-bold">
+          Order Here
+        </button>
+      </div>
+      <div>
+        <img
+          src={dynoRight}
+          alt="Dyno Right"
+          className="absolute right-0 bottom-0"
+        />
+        <img
+          src={dynoLeft}
+          alt="Dyno Left"
+          className="absolute left-0 bottom-0"
+        />
+      </div>
     </div>
   );
 };
